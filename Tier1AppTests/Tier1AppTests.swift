fc23ec53a7d17d345434e4983bf9870efc7c05d2
Conrawr422 <connercress@gmail.com>
2017-04-04 10:25:39 -0500

    Initial Commit

---
+++ Tier1AppTests/Tier1AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tier1AppTests.swift
+//  Tier1AppTests
+//
+//  Created by CRESS, CONNER on 4/4/17.
+//  Copyright © 2017 CRESS, CONNER. All rights reserved.
+//
+
+import XCTest
+@testable import Tier1App
+
+class Tier1AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
